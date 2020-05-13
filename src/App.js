77cee77cb20ecd6e6741ff40ca5b60dab6edad16
AJ <alexanderjessediaz@gmail.com>
2020-05-13 14:44:57 -0600

    fetch call to backend

--- src/App.js
+++ src/App.js
@@ -1,27 +1,47 @@
 import React, { Component } from 'react'
 import './App.css'
-import { response } from 'express';
+// import { response } from 'express'
+// import { Client } from 'espn-fantasy-football-api'
+
+// const myClient = new Client({leagueId: 20470559})
+// console.log(myClient)
+
+// let data = {}
+
+// myClient.getHistoricalScoreboardForWeek({seasonId:2019, scoringPeriodId:13, matchupPeriodId:13}).then((boxscores) => {
+  //     data = boxscores
+  // })
+  // console.log(data) 
+
+
 
 class App extends Component {
-
+  
   state = {
-    placeholder: []
+    data: []
   }
 
   componentDidMount(){
-    fetch("http://localhost:9000")
-      .then(response => response.json())
-      .then(espnObject => this.setState({
-        placeholder:espnObject
-      }))
-  }
-
-  render(){
+    fetch("http://localhost:9000",
+    {method:"GET"})
+        .then(response => response.json())
+        .then(allNFLPlayerObject => this.setState({
+          data:allNFLPlayerObject
+        })).catch((error) => {
+          console.error(error)
+        })
+    }
+    
+    
+    
+    render(){
+      console.log(this.state.data)
     return (
       <div className="App">
       </div>
     );
   }
 }
+
 
 export default App;
